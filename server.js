c1eb9581603611d3109852a29d3b48f98c085c84
renzo1704 <renzo@valere.io>
2023-05-12 08:32:46 -0500

    file changed

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express=require('express');
 const Gun=require('gun');
 const app=express();
-const port=8080;
+const port=process.env.PORT||8080;
 
 
 app.use(Gun.serve);
+
 app.get('/',(req,res)=>{
     res.send(`Relay peer en línea en el puerto ${port}`);
 })
 const server=app.listen(port,()=>{
     console.log(`Listening at port localhost:${port}`);
 })
-Gun({file:'db/data',web:server});
+
+const gun= Gun({web:server,file:'db/data'});
+var cont=0;
+
+gun.on('in', (msg) => {
+    console.log('Conexión entrante:', ++cont);
+  });
+  
